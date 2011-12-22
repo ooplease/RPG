df6c340e4af58a7258c73f4cc96b4ae92c47d70e
Ryan Hornik <Ryanhornik@gmail.com>
2011-12-22 03:21:44 -0600

    Commit 4

--- Game/RPG_attempt1/src/rpg/game/MainGame/MainGame.java
+++ Game/RPG_attempt1/src/rpg/game/MainGame/MainGame.java
@@ -1,8 +1,12 @@
 package rpg.game.MainGame;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
+import rpg.game.MainGame.States.*;
 
 public class MainGame extends StateBasedGame {
 	
@@ -10,15 +14,34 @@ public class MainGame extends StateBasedGame {
 	public final int OPTIONS = 1;
 	public final int LOADMENU = 2;
 	public final int INTROMOVIE = 3;
+	//TODO add any additional states
 
-	public MainGame(String name) {
-		super(name);
-		// TODO Auto-generated constructor stub
+	public MainGame() {
+		super("RPG");
+		
+		this.addState(new MainMenu(MAINMENU));
+		//TODO add all states
+		
+		this.enterState(MAINMENU);
+	}
+	
+	public static void main(String[] args) throws SlickException {
+		AppGameContainer app = new AppGameContainer(new MainGame());
+		Toolkit toolkit =  Toolkit.getDefaultToolkit ();
+		Dimension dim = toolkit.getScreenSize();
+		
+		
+		app.setDisplayMode((int)dim.getWidth(),(int)dim.getHeight(),true);
+		app.start();
 	}
 
 	@Override
-	public void initStatesList(GameContainer arg0) throws SlickException {
-		// TODO Auto-generated method stub
+	public void initStatesList(GameContainer gc) throws SlickException {
+		this.getState(MAINMENU).init(gc, this);
+//		this.getState(OPTIONS).init(gc, this);
+//		this.getState(LOADMENU).init(gc, this);
+//		this.getState(INTROMOVIE).init(gc, this);
+		//TODO add any additional states
 
 	}
 
---
+++ Game/RPG_attempt1/src/rpg/game/MainGame/States/MainMenu.java
@@ -0,0 +1,117 @@
+package rpg.game.MainGame.States;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+import rpg.game.MainGame.MainGame;
+
+public class MainMenu extends BasicGameState {
+	
+	int stateID = -1;
+	Image background = null;
+	Image options = null;
+	Image newgame = null;
+	Image loadgame = null;
+	
+	boolean overNewGame;
+	boolean overLoadGame;
+	boolean overOptions;
+	boolean overExit;
+
+	public MainMenu(int sID) {
+		stateID = sID;
+	}
+
+	@Override
+	public void init(GameContainer gc, StateBasedGame sbg)
+			throws SlickException {
+		background = new Image("res/MenuBKG.jpg");
+		Image menuOptions = new Image("res/MenuButtons.png");
+		// new game is 0,0,295,62
+		// load game is 0,67,302,130-67
+		// options 0,131,211,192-131
+		newgame = menuOptions.getSubImage(0, 0, 295, 62);
+		loadgame = menuOptions.getSubImage(0, 67, 302, 130-67);
+		options = menuOptions.getSubImage(0, 131, 211, 192-131);
+		
+
+	}
+
+	@Override
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
+			throws SlickException {
+		background.draw(0,0);
+		if(overNewGame){
+			g.setColor(Color.white);
+			g.fillRect(200,600,newgame.getWidth(),newgame.getHeight());
+			g.setColor(Color.black);
+		}
+		newgame.draw(200,600);
+		if(overLoadGame){
+			g.setColor(Color.white);
+			g.fillRect(200,700,loadgame.getWidth(),loadgame.getHeight());
+			g.setColor(Color.black);
+		}
+		loadgame.draw(200,700);
+		if(overOptions){
+			g.setColor(Color.white);
+			g.fillRect(200,800,options.getWidth(),options.getHeight());
+			g.setColor(Color.black);
+		}
+		options.draw(200,800);
+		if(overExit)
+			g.setColor(Color.white);
+		else
+			g.setColor(Color.black);
+		g.fillRect(200,900,200,100);
+		g.drawString("EXIT", 250, 925);
+
+	}
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sbg, int delta)
+			throws SlickException {
+		Input in = gc.getInput();
+		
+		int mX = in.getMouseX();
+		int mY = in.getMouseY();
+		
+		overNewGame = false;
+		overLoadGame = false;
+		overOptions = false;
+		overExit = false;
+		
+		if((mX >= 200 && mX <= 495)&&(mY >= 600 && mY <= 662))
+			overNewGame = true;
+		if((mX >= 200 && mX <= 502)&&(mY >= 700 && mY <= 700+(130-67)))
+			overLoadGame = true;
+		if((mX >= 200 && mX <= 411)&&(mY >= 800 && mY <= 800+(192-131)))
+			overOptions = true;
+		if((mX >= 200 && mX <= 400)&&(mY >= 900 && mY <= 1000))
+			overExit = true;
+		
+		
+//		if(overNewGame && in.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON))
+//			sbg.enterState(3);
+//		if(overLoadGame && in.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON))
+//			sbg.enterState(2);
+//		if(overOptions && in.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON))
+//			sbg.enterState(1);
+		if(overExit && in.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON))
+			gc.exit();
+
+	}
+
+	@Override
+	public int getID() {
+		
+		return stateID;
+	}
+
+}
